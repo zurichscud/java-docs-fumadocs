d14b6ba24b3a3e5d26c43c79311cc506082bc2cc
laibinglin <1154246882@qq.com>
2026-08-07 14:25:19 +0800

    更新布局

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,5 +1,5 @@
 import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import { baseOptions } from '@/lib/layout.shared';
 import { AISearch, AISearchPanel, AISearchTrigger } from '@/components/ai/search';
 import { MessageCircleIcon } from 'lucide-react';
@@ -8,7 +8,11 @@ import { buttonVariants } from 'fumadocs-ui/components/ui/button';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout
+      {...baseOptions()}
+      tabMode="navbar"
+      tree={source.getPageTree()}
+    >
       <AISearch>
         <AISearchPanel />
         <AISearchTrigger
