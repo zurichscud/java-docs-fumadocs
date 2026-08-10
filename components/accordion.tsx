d91882bf4d469b4e4b756f72118c8d29cd7791c6
laibinglin <1154246882@qq.com>
2026-08-10 17:56:33 +0800

    update

---
+++ components/accordion.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { Check, LinkIcon } from 'lucide-react';
+import { type ComponentProps, type ReactNode, useEffect, useRef, useState } from 'react';
+import { cn } from '../lib/cn';
+import { useCopyButton } from '@fumadocs/base-ui/utils/use-copy-button';
+import { buttonVariants } from './ui/button';
+import { mergeRefs } from '../lib/merge-refs';
+import { useTranslations } from '@fuma-translate/react';
+import {
+  Accordion as Root,
+  AccordionContent,
+  AccordionHeader,
+  AccordionItem,
+  AccordionTrigger,
+} from './ui/accordion';
+
+export function Accordions({
+  ref,
+  className,
+  defaultValue,
+  ...props
+}: ComponentProps<typeof Root>) {
+  const rootRef = useRef<HTMLDivElement>(null);
+  const composedRef = mergeRefs(ref, rootRef);
+  const [value, setValue] = useState<unknown[]>(defaultValue ?? []);
+
+  useEffect(() => {
+    const id = window.location.hash.substring(1);
+    const element = rootRef.current;
+    if (!element || id.length === 0) return;
+
+    const selected = document.getElementById(id);
+    if (!selected || !element.contains(selected)) return;
+    const value = selected.getAttribute('data-accordion-value');
+
+    if (value) setValue((prev) => [value, ...prev]);
+  }, []);
+
+  return (
+    <Root
+      ref={composedRef}
+      value={value}
+      onValueChange={setValue}
+      className={(s) =>
+        cn(
+          'divide-y divide-fd-border overflow-hidden rounded-lg border bg-fd-card',
+          typeof className === 'function' ? className(s) : className,
+        )
+      }
+      {...props}
+    />
+  );
+}
+
+export function Accordion({
+  title,
+  id,
+  value = String(title),
+  children,
+  ...props
+}: Omit<ComponentProps<typeof AccordionItem>, 'value' | 'title'> & {
+  title: string | ReactNode;
+  value?: string;
+}) {
+  return (
+    <AccordionItem value={value} {...props}>
+      <AccordionHeader id={id} data-accordion-value={value}>
+        <AccordionTrigger>{title}</AccordionTrigger>
+        {id ? <CopyButton id={id} /> : null}
+      </AccordionHeader>
+      <AccordionContent hiddenUntilFound>
+        <div className="px-4 pb-2 text-[0.9375rem] prose-no-margin [&[hidden]:not([hidden='until-found'])]:hidden">
+          {children}
+        </div>
+      </AccordionContent>
+    </AccordionItem>
+  );
+}
+
+function CopyButton({ id }: { id: string }) {
+  const t = useTranslations({ note: 'accordion' });
+  const [checked, onClick] = useCopyButton(() => {
+    const url = new URL(window.location.href);
+    url.hash = id;
+
+    return navigator.clipboard.writeText(url.toString());
+  });
+
+  return (
+    <button
+      type="button"
+      aria-label={t('Copy Link', { note: 'aria-label' })}
+      className={cn(
+        buttonVariants({
+          color: 'ghost',
+          className: 'text-fd-muted-foreground me-2',
+        }),
+      )}
+      onClick={onClick}
+    >
+      {checked ? <Check className="size-3.5" /> : <LinkIcon className="size-3.5" />}
+    </button>
+  );
+}
